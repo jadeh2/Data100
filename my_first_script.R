449a6cdac2fde7a521ac8e0e7445a88b4ba10d63
jadeh2 <69915041+jadeh2@users.noreply.github.com>
2020-08-23 16:36:34 -0400

    Add files via upload
    Line plot of "The Path of a Running Boy". Defined x and y, then plotted them and added a title.

---
+++ my_first_script.R
@@ -0,0 +1,14 @@
+setwd("~/Desktop/project_folder_datascience/data")
+x <- 1:10  
+y <- 10:1
+plot(x,y)
+plot(x, y, type = "o",
+     main = "The Path of a Running Boy",
+     sub = "units of distance = meters",
+     xlab = "longitude",
+     ylab = "latitude",
+     lty = 2,
+     lwd = .75,
+     col = "blue",
+     pch = 0,
+     cex = 1.5)
